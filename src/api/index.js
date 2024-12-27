ed9ca7bc51cb21329655e9f001d7f7b85d093bba
DevNuxCode <devnuxcode@gmail.com>
2024-12-27 16:45:11 -0300

    index Api

---
+++ src/api/index.js
@@ -0,0 +1,7 @@
+require('dotenv').config(); // Load environment variables
+
+const app = require('../app');
+
+app.listen(app.get('port'), ()=> {
+    console.log("Servidor on localhost escuchando puerto ", app.get("port"));
+});
