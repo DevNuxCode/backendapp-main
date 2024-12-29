f68f53313054ded203a63d25b5a6b542a9ba36be
DevNuxCode <devnuxcode@gmail.com>
2024-12-29 01:59:57 -0300

    Updt index 2

--- src/index.js
+++ src/index.js
@@ -5,5 +5,4 @@ const app = require('./app');
 app.listen(app.get('port'), ()=> {
     console.log("Servidor on localhost escuchando puerto ", app.get("port"));
 });
-
-module.exports = app
+module.exports = index
